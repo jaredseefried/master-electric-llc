b9a3d466ec904f75e893bdd6425bfb5195b2a125
Jared Seefried <jseefried@conga.com>
2024-01-09 13:31:40 -0700

    Set up app and made navigation with logo

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import MasterElectricLogo from "../../images/Master-Electric-LLC.png";
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <>
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand
+            href="#home"
+            className="d-flex align-items-center home-link"
+          >
+            <img
+              alt="Master Electric LLC Logo"
+              src={MasterElectricLogo}
+              width="80"
+              className="d-inline-block align-top"
+            />{" "}
+            Master Electric, LLC
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
