81aa3de5727c18449257a8d4172ff0ed10eb4e88
Jared Seefried <jseefried@conga.com>
2024-01-15 13:35:13 -0700

    added solutions section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ContactBanner from "./components/ContactBanner/ContactBanner";
 import Navigation from "./components/navigation/Navigation";
 import Reviews from "./components/Reviews/Reviews";
 import Services from "./components/Services/Services";
+import Solutions from "./components/Solutions/Solutions";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Banner />
       <ContactBanner />
       <Services />
+      <Solutions />
       <Reviews />
     </div>
   );
---
+++ src/components/Solutions/Solutions.js
@@ -0,0 +1,96 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "../Solutions/Solutions.css";
+import { FaBolt } from "react-icons/fa6";
+import { FaPlugCircleCheck } from "react-icons/fa6";
+
+export default function Solutions() {
+  return (
+    <div className="lighting-list-container">
+      <h1 className="lighting-solutions">
+        <FaBolt id="bolt-icon" />
+        Lighting Solutions:
+      </h1>
+      <Container>
+        <Row>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Lighting Control System
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Panel Services
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Basement Remodels
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Kitchen and Bath{" "}
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Exterior and Garages
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Aluminum Wiring Rewire
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Breaker Installation
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Grounding Systems
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Surge Protection
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Smoke Detector Install
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              New Construction
+            </p>
+          </Col>
+          <Col className="lighting-list-col">
+            <p className="lighting-list">
+              <FaPlugCircleCheck className="circle-plug-icon" />
+              Troubleshooting
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
