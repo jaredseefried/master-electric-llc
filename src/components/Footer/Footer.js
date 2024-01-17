a9662278f7c83c1108f8e0df04b7e80a38cbc3f7
Jared Seefried <jseefried@conga.com>
2024-01-17 12:20:47 -0700

    fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,16 +9,22 @@ export default function Footer() {
       <Container>
         <Row>
           <Col>
-            <h1> Corey Divis</h1>
+            <h3 id="footer-name"> Master Electric, LLC</h3>
+            <p id="footer-description">
+              Master Electric, LLC provides residential and commercial
+              electrical solutions, serving the Denver Metro area.
+            </p>
           </Col>
           <Col>
-            <h2>720-339-5614</h2>
+            <h3 id="footer-label">Contact</h3>
+            <p id="footer-number">720-339-5614</p>
+            <p id="footer-email">masterelectricllc@outlook.com</p>
           </Col>
-          <Col>
-            <BsFacebook className="social-icons" />
-            <BsInstagram className="social-icons" />
-            <BsLinkedin className="social-icons" />
-            <BsYelp className="social-icons" />{" "}
+          <Col id="footer-social-icons-container" className="float-end">
+            <BsFacebook className="footer-social-icons" />
+            <BsInstagram className="footer-social-icons" />
+            <BsLinkedin className="footer-social-icons" />
+            <BsYelp className="footer-social-icons" />{" "}
           </Col>
         </Row>
       </Container>
